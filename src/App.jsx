7e9aea7321f90ddc505f769cfbd7a92e93fe642b
fadayopaul <paulfadayo@yahoo.com>
2024-09-03 23:03:04 +0100

    feat: Add react-router for routing and NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
