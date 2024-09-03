b0829b8c524395b84e54f424ff2358d5043accaa
fadayopaul <paulfadayo@yahoo.com>
2024-09-03 21:57:20 +0100

    chore: Update npm dependencies and add daisyui plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className="text-sm font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-sm font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Button</button>
+    </>
+  );
 }
 
 export default App;
