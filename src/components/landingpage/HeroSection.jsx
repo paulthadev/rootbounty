df45a64e8945ca6ee340343d7814b88b541850b1
fadayopaul <paulfadayo@yahoo.com>
2024-09-04 13:02:52 +0100

    chore: Update landing page HeroSection component and styles

--- src/components/landingpage/HeroSection.jsx
+++ src/components/landingpage/HeroSection.jsx
@@ -1,5 +1,6 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import heroBanner from "../../assets/hero-banner.png";
+import { heroSectionInfo } from "../../constants/landing";
 import { styles } from "../../styles/styles";
 
 const HeroSection = () => (
@@ -23,16 +24,20 @@ const HeroSection = () => (
 function MainHeading() {
   return (
     <div className="max-w-[15rem]  xs:max-w-[20rem] md:max-w-[25rem]  lg:max-w-[30rem] grid gap-y-8 lg:gap-y-10">
-      <div className="grid gap-y-3 xs:gap-y-4 lg:gap-y-7 text-white">
-        <h1 className="text-2xl leading-none font-extrabold xs:text-3xl sm:text-[2rem] md:text-4xl lg:text-5xl lg:leading-none">
-          Fresh from Earth, available within your reach.
-        </h1>
+      {heroSectionInfo.map((items) => (
+        <div
+          key={items.id}
+          className="grid gap-y-3 xs:gap-y-4 lg:gap-y-7 text-white"
+        >
+          <h1 className="text-2xl leading-none font-extrabold xs:text-3xl sm:text-[2rem] md:text-4xl lg:text-5xl lg:leading-none">
+            {items.title}
+          </h1>
 
-        <h2 className="text-xs xs:text-base xs:leading-tight font-medium md:text-xl lg:text-2xl">
-          Discover the best selection of tuber crops, sourced directly from
-          farmers around you.
-        </h2>
-      </div>
+          <h2 className="text-xs xs:text-base xs:leading-tight font-medium md:text-xl lg:text-2xl">
+            {items.subTitle}
+          </h2>
+        </div>
+      ))}
 
       <div className="flex gap-x-4 xs:gap-x-7 text-white">
         <button className="text-xs xs:text-sm md:text-base bg-[#4CAF50] hover:bg-[#146317] px-4 py-2 rounded-md transition-all duration-300 lg:text-lg">
---
+++ src/constants/landing.js
@@ -0,0 +1,8 @@
+export const heroSectionInfo = [
+  {
+    id: 1,
+    title: "Fresh from Earth, available within your reach.",
+    subTitle:
+      "Discover the best selection of tuber crops, sourced directly from farmers around you.",
+  },
+];
