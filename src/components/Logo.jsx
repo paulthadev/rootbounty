f0d34448c55b5f5e55ff00a34c3c2c6dc6375b38
fadayopaul <paulfadayo@yahoo.com>
2024-09-04 12:28:51 +0100

    chore: Update landing page Header and HeroSection components and styles

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,9 @@
 function Logo() {
   return (
     <div>
-      <p className="text-[#4CAF50] font-extrabold text-xl">RootBounty</p>
+      <p className="text-[#4CAF50] font-extrabold text-xl lg:text-3xl">
+        RootBounty
+      </p>
     </div>
   );
 }
--- src/components/landingpage/HeroSection.jsx
+++ src/components/landingpage/HeroSection.jsx
@@ -2,16 +2,18 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import heroBanner from "../../assets/hero-banner.png";
 
 const HeroSection = () => (
-  <section className="h-fit flex flex-col">
+  <section className="h-fit flex scrollbar-hide flex-col">
     <div className="relative">
-      <div className="absolute inset-0 bg-black opacity-80 "></div>
+      <div className="absolute inset-0 top-0 bg-black opacity-80 "></div>
       <LazyLoadImage
         src={heroBanner}
-        className="w-full h-[20rem] xs:h-[25rem] md:h-[30rem] object-cover"
+        className="w-full h-[20rem] xs:h-[25rem] lg:h-[35rem] md:h-[30rem] object-cover xl:h-[45rem]"
       />
     </div>
 
-    <div className={`absolute mt-12 sm:mt-16 px-6 md:mt-24 md:mx-10 max-w-7xl`}>
+    <div
+      className={`absolute mt-12 sm:mt-16 px-6 md:mt-24 md:mx-10 max-w-7xl lg:mt-36`}
+    >
       <MainHeading />
     </div>
   </section>
@@ -19,23 +21,24 @@ const HeroSection = () => (
 
 function MainHeading() {
   return (
-    <div className="max-w-[15rem] xs:max-w-[20rem] md:max-w-[25rem] grid gap-y-8">
-      <div className="grid gap-y-3 xs:gap-y-4 text-white">
-        <h1 className="text-2xl leading-none font-extrabold xs:text-3xl sm:text-[2rem] md:text-4xl">
+    <div className="max-w-[15rem]  xs:max-w-[20rem] md:max-w-[25rem]  lg:max-w-[30rem] grid gap-y-8 lg:gap-y-10">
+      <div className="grid gap-y-3 xs:gap-y-4 lg:gap-y-7 text-white">
+        <h1 className="text-2xl leading-none font-extrabold xs:text-3xl sm:text-[2rem] md:text-4xl lg:text-5xl lg:leading-none">
           Fresh from Earth, available within your reach.
         </h1>
 
-        <h2 className="text-xs xs:text-base xs:leading-tight font-medium md:text-xl">
+        <h2 className="text-xs xs:text-base xs:leading-tight font-medium md:text-xl lg:text-2xl">
           Discover the best selection of tuber crops, sourced directly from
           farmers around you.
         </h2>
       </div>
 
       <div className="flex gap-x-4 xs:gap-x-7 text-white">
-        <button className="text-xs xs:text-sm md:text-base bg-[#4CAF50] hover:bg-[#146317] px-4 py-2 rounded-md transition-all duration-300">
+        <button className="text-xs xs:text-sm md:text-base bg-[#4CAF50] hover:bg-[#146317] px-4 py-2 rounded-md transition-all duration-300 lg:text-lg">
           Join now
         </button>
-        <button className="text-xs hover:bg-neutral-950 hover:text-white hover:border-neutral-950 px-4 py-2 xs:text-sm border border-white rounded-md transition-all duration-300">
+
+        <button className="text-xs hover:bg-neutral-950 hover:text-white hover:border-neutral-950 px-4 py-2 xs:text-sm border border-white rounded-md transition-all duration-300 lg:text-lg">
           Login as guest
         </button>
       </div>
