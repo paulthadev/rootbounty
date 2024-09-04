f9791a4d34f0414745aa4931eb88a59be36eda17
fadayopaul <paulfadayo@yahoo.com>
2024-09-04 01:17:26 +0100

    chore: Add Logo component and update Navbar to include logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div>
+      <p className="text-[#4CAF50] font-extrabold text-3xl">RootBounty</p>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/styles/styles.js
@@ -0,0 +1,3 @@
+export const styles = {
+  maxWidth: "max-w-7xl px-6 md:px-6",
+};
