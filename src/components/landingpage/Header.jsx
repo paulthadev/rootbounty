92669529b872721acd353e80f9a7ed9a38d3ffd4
fadayopaul <paulfadayo@yahoo.com>
2024-09-04 11:58:38 +0100

    chore: Update landing page Header and HeroSection components and styles

--- src/components/landingpage/Header.jsx
+++ src/components/landingpage/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../Logo";
 function Header() {
   return (
     <header className="w-full sticky top-0 z-50 bg-white/95  backdrop-blur-md h-fit">
-      <div className="max-w-7xl mx-auto px-6 py-3 flex items-center">
+      <div className="max-w-7xl mx-auto md:mx-10 px-6 py-3 flex items-center">
         <Logo />
       </div>
     </header>
