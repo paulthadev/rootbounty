f6cea8faa0dcb1f8a908b94313612215e635913b
fadayopaul <paulfadayo@yahoo.com>
2024-09-04 10:22:45 +0100

    chore: Update landing page HeroSection component and styles

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,17 +1,12 @@
 import Header from "../components/landingpage/Header";
 import HeroSection from "../components/landingpage/HeroSection";
-import WorkShowcase from "../components/landingpage/WorkShowcase";
-import { styles } from "../styles/styles";
 
 function LandingPage() {
   return (
-    <div>
+    <>
       <Header />
-      <div className={`${styles.maxWidth}`}>
-        <HeroSection />
-        <WorkShowcase />
-      </div>
-    </div>
+      <HeroSection />
+    </>
   );
 }
 
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,6 +1,5 @@
 export const styles = {
-  maxWidth: "max-w-7xl mx-auto px-6 md:px-6",
-  NavWidth: "max-w-7xl px-6 md:px-6",
+  maxWidth: "max-w-7xl mx-auto px-6",
   gridCenter: "grid place-items-center",
 
   dashMaxWidth: "max-w-[90rem] mx-auto",
