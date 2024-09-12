bc0a13be80e482fa9ac1a442415a642538fd741c
fadayopaul <paulfadayo@yahoo.com>
2024-09-12 12:05:58 +0100

    Refactor CSS to style registration input for autofill

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
+
 function Inputs({ type, placeholder, className, name, onChange }) {
   return (
-    <div className="py-2">
+    <div className="p-2 ">
       <input
         type={type}
         name={name}
         placeholder={placeholder}
         onChange={onChange}
-        className={`${className} bg-white border outline-none border-gray-400 w-full p-2 rounded-lg text-gray-900`}
+        className={`registrationinput ${className} bg-white border outline-none focus:border-gray-400 input border-gray-400 w-full rounded-lg text-gray-900`}
         required
       />
     </div>
