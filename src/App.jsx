d6cc856c3e85453bb0a1cc489595d6689845c4b5
fadayopaul <paulfadayo@yahoo.com>
2024-09-12 08:45:07 +0100

    Add Onboarding page to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
+import Onboarding from "./pages/Onboarding";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" index element={<LandingPage />} />
+        <Route path="/onboarding" index element={<Onboarding />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
