b33c81c1dc046eba1b5fd0a44bcc01f09004efb3
fadayopaul <paulfadayo@yahoo.com>
2024-09-12 12:06:10 +0100

    Refactor SignUpFarmer page component to improve styling and responsiveness

--- src/pages/SignUpFarmer.jsx
+++ src/pages/SignUpFarmer.jsx
@@ -1,19 +1,16 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import heroBanner from "../assets/hero-banner.png";
 import { styles } from "../styles/styles";
-
 import Header from "../components/landingpage/Header";
 import Inputs from "../components/Inputs";
 import { useState } from "react";
 
 const SignUpFarmer = () => (
-  <section className="relative flex flex-col h-[90vh]scrollbar-hide">
-    {/* Set z-index and padding to avoid overlap */}
+  <section className="relative flex flex-col ">
     <Header className="z-10 relative" />
 
-    <div className="flex flex-col">
+    <div className="flex flex-col  ">
       <div className="relative w-full h-screen">
-        {/* Background image and overlay */}
         <div className="absolute h-screen inset-0 bg-black opacity-80"></div>
         <LazyLoadImage
           src={heroBanner}
@@ -22,16 +19,14 @@ const SignUpFarmer = () => (
       </div>
 
       {/* Content section with space for the header */}
-      <div className="absolute inset-0 flex gap-y-10 flex-col justify-between pt-24">
-        {/* Heading centered */}
-        <div className="flex flex-col items-center ">
+      <div className="absolute inset-0 flex lg:items-center  lg:flex-row gap-y-10 flex-col justify-between pt-24 lg:pt-0">
+        <div className="flex lg:w-1/2 flex-col items-center ">
           <div className={`${styles.maxWidth}`}>
             <Heading />
           </div>
         </div>
 
-        {/* Register Section pushed to the bottom */}
-        <div className="w-full">
+        <div className="lg:w-1/2 w-full flex justify-center flex-col lg:self-stretch bg-white">
           <RegisterSection />
         </div>
       </div>
@@ -42,12 +37,12 @@ const SignUpFarmer = () => (
 function Heading() {
   return (
     <div className="text-white w-full">
-      <h1 className="text-3xl font-bold max-w-sm mb-4">
+      <h1 className="text-3xl font-bold max-w-sm mb-4 lg:text-5xl lg:leading-tight">
         Welcome, <br />
         Let&apos;s Get You Started
       </h1>
 
-      <p className="text-xl leading-tight">
+      <p className="text-xl leading-tight lg:text-2xl">
         Join our platform and start selling your high quality tuber crops.
       </p>
     </div>
@@ -86,14 +81,14 @@ function RegisterSection() {
   };
 
   return (
-    <div className="w-full bg-white p-8 md:p-12">
-      <h1 className="text-black font-semibold pb-4 text-xl">
+    <div className="w-full bg-white p-8 md:p-12 ">
+      <h1 className="text-black  lg:text-3xl font-semibold pb-4 text-xl">
         Create New Account
       </h1>
 
       <form
         onSubmit={handleSubmit}
-        className="grid grid-cols-1 md:grid-cols-2 gap-2"
+        className="grid md:grid-cols-2 gap-2 lg:flex lg:flex-col"
       >
         <Inputs
           type="text"
@@ -134,10 +129,9 @@ function RegisterSection() {
           value={formData.phone}
           onChange={handleChange}
         />
-
         <select
           name="tuber"
-          className="bg-white border outline-none border-gray-400 w-full p-2 rounded-lg text-gray-900"
+          className="select bg-white border outline-none border-gray-400 w-full px-2 focus:border-gray-400 rounded-lg text-gray-900"
           value={formData.tuber}
           onChange={handleChange}
         >
