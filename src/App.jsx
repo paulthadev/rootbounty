ca9433de63de8c46f39bcfdedfaf922e6666e5ac
fadayopaul <paulfadayo@yahoo.com>
2024-09-12 10:00:25 +0100

    Add SignUpFarmer and SignUpBuyer pages to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import Onboarding from "./pages/Onboarding";
+import SignUpFarmer from "./pages/SignUpFarmer";
+import SignUpBuyer from "./pages/SignUpBuyer";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" index element={<LandingPage />} />
         <Route path="/onboarding" index element={<Onboarding />} />
+        <Route path="/signupfarmer" element={<SignUpFarmer />} />
+        <Route path="/signupbuyer" element={<SignUpBuyer />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
