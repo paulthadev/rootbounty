0d6babfc35bda355f06ffa70acf49fc190fb86de
fadayopaul <paulfadayo@yahoo.com>
2024-09-13 22:29:35 +0100

    Add formatDate utility function for date formatting

---
+++ src/utils/formatDate.js
@@ -0,0 +1,18 @@
+import { formatDistanceToNow } from "date-fns";
+
+export const formatDate = (date) => {
+  if (!date) return "";
+
+  const distance = formatDistanceToNow(new Date(date), { addSuffix: true });
+  return distance
+    .replace("minute", "min")
+    .replace("minutes", "min")
+    .replace("hour", "hr")
+    .replace("hours", "hrs")
+    .replace("day", "d")
+    .replace("days", "d")
+    .replace("month", "mo")
+    .replace("months", "mo")
+    .replace("year", "yr")
+    .replace("years", "yr");
+};
