406dbfca5375aec4da73e98475afa79dbf91faa4
fadayopaul <paulfadayo@yahoo.com>
2024-09-13 16:30:48 +0100

    Refactor react-router to react-router-dom for routing in App.jsx
    Co-Authored-By: Victor olabisi <oguntayovictor65@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import Onboarding from "./pages/Onboarding";
@@ -6,85 +6,80 @@ import SignUpFarmer from "./pages/auth/SignUpFarmer";
 import SignUpBuyer from "./pages/auth/SignUpBuyer";
 import LoginPage from "./pages/auth/LoginPage";
 import UserProfile from "./pages/dashboard/UserProfile";
-import AuthCheck from "./routes/AuthCheck";
-import ProtectedRoute from "./routes/ProtectedRoutes";
 import BuyerDashboard from "./pages/dashboard/BuyerDashboard";
 import FarmerDashboard from "./pages/dashboard/FarmerDashboard";
+import AuthCheck from "./routes/AuthCheck";
+import ProtectedRoute from "./routes/ProtectedRoutes";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/onboarding",
+    element: (
+      <AuthCheck>
+        <Onboarding />
+      </AuthCheck>
+    ),
+  },
+  {
+    path: "/signupfarmer",
+    element: (
+      <AuthCheck>
+        <SignUpFarmer />
+      </AuthCheck>
+    ),
+  },
+  {
+    path: "/signupbuyer",
+    element: (
+      <AuthCheck>
+        <SignUpBuyer />
+      </AuthCheck>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <AuthCheck>
+        <LoginPage />
+      </AuthCheck>
+    ),
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedRoute>
+        <UserProfile />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/buyer/dashboard",
+    element: (
+      <ProtectedRoute>
+        <BuyerDashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/farmer/dashboard",
+    element: (
+      <ProtectedRoute>
+        <FarmerDashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" index element={<LandingPage />} />
-
-        {/* Wrap restricted routes with AuthCheck */}
-        <Route
-          path="/onboarding"
-          index
-          element={
-            <AuthCheck>
-              <Onboarding />
-            </AuthCheck>
-          }
-        />
-        <Route
-          path="/signupfarmer"
-          element={
-            <AuthCheck>
-              <SignUpFarmer />
-            </AuthCheck>
-          }
-        />
-        <Route
-          path="/signupbuyer"
-          element={
-            <AuthCheck>
-              <SignUpBuyer />
-            </AuthCheck>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <AuthCheck>
-              <LoginPage />
-            </AuthCheck>
-          }
-        />
-
-        {/* Protect profile route for logged-in users only */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <UserProfile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/buyer/dashboard"
-          element={
-            <ProtectedRoute>
-              <BuyerDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/farmer/dashboard"
-          element={
-            <ProtectedRoute>
-              <FarmerDashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Public routes */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
