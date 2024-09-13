29aa36dee847134884976ea301d41a86e675a808
fadayopaul <paulfadayo@yahoo.com>
2024-09-13 22:29:55 +0100

    Refactor Products component to fetch and display products dynamically

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,93 +1,86 @@
 import { Link } from "react-router-dom";
 import potatoe from "/potatoe.png";
-
-const allProducts = [
-  {
-    id: 1,
-    companyName: "   Agrictech Farm",
-    productName: "irish potatoe",
-    location: "akure Nigeria",
-    image: potatoe,
-    price: 5000,
-    description:
-      "Freshly harvested and ready for you! 🌿🌾 Just pulled these sweet potatoes straight from the farm today.They’re 100% organic,",
-    time: "3 hours ago",
-  },
-  {
-    id: 2,
-    companyName: "   Agrictech Farm",
-    productName: "irish potatoe",
-    location: "akure Nigeria",
-    image: potatoe,
-    price: 5000,
-    description:
-      "Freshly harvested and ready for you! 🌿🌾 Just pulled these sweet potatoes straight from the farm today.They’re 100% organic, ",
-    time: "3 hours ago",
-  },
-  {
-    id: 3,
-    companyName: "   Agrictech Farm",
-    productName: "irish potatoe",
-    location: "akure Nigeria",
-    image: potatoe,
-    price: 5000,
-    description:
-      "Freshly harvested and ready for you! 🌿🌾 Just pulled these sweet potatoes straight from the farm today.They’re 100% organic,",
-    time: "3 hours ago",
-  },
-  {
-    id: 4,
-    companyName: "   Agrictech Farm",
-    productName: "irish potatoe",
-    location: "akure Nigeria",
-    image: potatoe,
-    price: 5000,
-    description:
-      "Freshly harvested and ready for you! 🌿🌾 Just pulled these sweet potatoes straight from the farm today.They’re 100% organic,",
-    time: "3 hours ago",
-  },
-];
+import Spinner from "./Spinner";
+import { supabase } from "../utils/supabase";
+import { useEffect, useState } from "react";
+import { formatDate } from "../utils/formatDate";
+import formatPrice from "../utils/formatPrice";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data, error } = await supabase.from("product").select("*");
+
+        if (error) {
+          throw error;
+        }
+
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) return <Spinner />;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <section>
       <div className="grid md:grid-cols-2 gap-4  lg:grid-cols-3 ">
-        {allProducts.map((product) => {
+        {products.map((product) => {
           const {
-            productName,
+            product_name,
+            product_id,
             location,
             image,
-            companyName,
+            company_name,
             price,
-            time,
+            created_at,
             description,
           } = product;
 
           return (
-            <article key={product.id} className="px-6 bg-white rounded-lg">
-              <div className="flex justify-between mt-[3rem] mb-[1.69rem]">
+            <article key={product_id} className="px-6 bg-white rounded-lg">
+              <div className="flex justify-between items-center mt-[3rem] mb-[1.69rem]">
                 <div className="">
                   <h2 className="font-bold text-[#1E1E1E] capitalize text-[1.25rem]">
-                    {companyName}
+                    {company_name}
                   </h2>
-                  <p className="text-[.625rem] ">{location}</p>
+                  <p className="text-[.625rem] max-w-[11rem] ">{location}</p>
                 </div>
-                <p className="text-[.625rem]">{time}</p>
+
+                <p className="text-[.625rem] xs:text-[.75rem] font-semibold">
+                  {formatDate(created_at)}
+                </p>
               </div>
-              <Link to={`${productName}`}>
-                <img src={image} alt="" className="w-[100%]" />
+              <Link to={`${product_name}`}>
+                <img src={potatoe} alt="" className="w-[100%]" />
               </Link>
+
               <div className="flex justify-between mt-4 items-center">
                 <h2 className="uppercase text-[1.2rem] md:text-[1.5rem] text-[#1E1E1E] font-bold">
-                  {productName}
+                  {product_name}
                 </h2>
-                <p className="text-[.925rem]">{price}</p>
+                <p className="text-[.925rem] font-semibold">
+                  {formatPrice(price)}
+                </p>
               </div>
+
               <p className="mb-4">
                 {description}
-                <Link to={`${productName}`}>
-                  <span className="text-[#4CAF50]">see more</span>
-                </Link>{" "}
+                <Link to={`${product_name}`}>
+                  <span className="text-[#4CAF50]"> see more</span>
+                </Link>
               </p>
             </article>
           );
