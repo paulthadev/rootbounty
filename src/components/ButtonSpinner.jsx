dcb96d92fd108df596c4c1f2af52cc39164ba786
fadayopaul <paulfadayo@yahoo.com>
2024-09-13 20:23:57 +0100

    Add ButtonSpinner component

---
+++ src/components/ButtonSpinner.jsx
@@ -0,0 +1,9 @@
+function ButtonSpinner() {
+  return (
+    <div className="grid place-items-center">
+      <div className="loading loading-spinner bg-blue-950"></div>
+    </div>
+  );
+}
+
+export default ButtonSpinner;
