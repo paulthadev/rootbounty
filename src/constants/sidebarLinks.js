f15467ed3a1faeab3081e3253dd00e5d79d4e361
victor <oguntayovictor65@gmail.com>
2024-09-13 17:43:52 -0700

    fix bugs

---
+++ src/constants/sidebarLinks.js
@@ -0,0 +1,22 @@
+const links = [
+  {
+    subtitle: "overview",
+    items: [
+      {
+        id: 1,
+        title: "products",
+        link: "/dashboard",
+      },
+      {
+        id: 2,
+        title: "orders",
+        link: "/orders",
+      },
+      {
+        id: 3,
+        title: "carts",
+        link: "/cart",
+      },
+    ],
+  },
+];
