6621730da74c46adf6ed2692fd16aea09b71d4c3
fadayopaul <paulfadayo@yahoo.com>
2024-09-15 22:01:22 +0100

    Refactor kg selector to display weight with a space before 'kg'

--- src/components/dashboard/KgSelector.jsx
+++ src/components/dashboard/KgSelector.jsx
@@ -3,7 +3,7 @@ const KgSelector = ({ selectedKg }) => {
   return (
     <div className="flex items-center mt-4">
       <div className="px-4 py-2 border rounded-full text-lg font-semibold bg-green-500 text-white">
-        {selectedKg}kg
+        {selectedKg} kg
       </div>
     </div>
   );
