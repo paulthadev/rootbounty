eef344f29a6bb44ec7ef36150798ceccd5711a32
fadayopaul <paulfadayo@yahoo.com>
2024-09-15 20:53:21 +0100

    Refactor dashboard components: Add KgSelector and ProgressBar components

---
+++ src/components/dashboard/KgSelector.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const KgSelector = ({ selectedKg }) => {
+  return (
+    <div className="flex items-center mt-4">
+      <div className="px-4 py-2 border rounded-full text-lg font-semibold bg-green-500 text-white">
+        {selectedKg}kg
+      </div>
+    </div>
+  );
+};
+
+export default KgSelector;
