f6e4c968f870df8efacdeff8fd56f8e0d911a1ad
fadayopaul <paulfadayo@yahoo.com>
2024-09-15 02:52:21 +0100

    Refactor ContactFarmerModal component to fetch and display farmer information

--- src/components/dashboard/ContactFarmerModal.jsx
+++ src/components/dashboard/ContactFarmerModal.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/prop-types */
-import { useEffect } from "react";
-import { BiPhone } from "react-icons/bi";
-import { CgClose } from "react-icons/cg";
-import Inputs from "../Inputs";
+import { useEffect, useState } from "react";
+import { CgClose, CgMail } from "react-icons/cg";
+import { supabase } from "../../utils/supabase";
+import { IoCall } from "react-icons/io5";
+import toast from "react-hot-toast";
 
 const ContactFarmerModal = ({ product, closeModal }) => {
+  const { farmer_id } = product;
+  const [farmer, setFarmer] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const { business_name, location, email, tuber, phone } = farmer;
+
   useEffect(() => {
     const dialog = document.getElementById("contactFarmerModal");
     if (dialog) {
@@ -18,25 +24,73 @@ const ContactFarmerModal = ({ product, closeModal }) => {
     };
   }, []);
 
-  console.log(product);
+  const fetchFarmer = async () => {
+    setIsLoading(true);
+    try {
+      const { data, error } = await supabase
+        .from("farmer")
+        .select("*")
+        .eq("farmer_id", farmer_id)
+        .single();
+
+      if (error) throw error;
+
+      setFarmer(data);
+    } catch (error) {
+      toast.error(`Error fetching farmer: ${error.message}`);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchFarmer();
+  }, []);
 
   return (
     <dialog id="contactFarmerModal" className="modal">
-      <div className="modal-box relative">
-        <h3 className="font-bold text-lg">Contact Abuja Farms</h3>
+      <div className="modal-box relative py-10">
+        {isLoading ? (
+          <div className="grid place-items-center">
+            <div className="loading loading-spinner bg-gray-700"></div>
+          </div>
+        ) : (
+          <>
+            <h3 className="font-bold text-2xl text-center">
+              Contact {business_name}
+            </h3>
+            <p className="text-center text-gray-500 capitalize">{location}</p>
 
-        <div className="mt-4 flex items-center gap-4 ">
-          <BiPhone />
-          <span className="p-2 border border-gray-800 rounded-lg">
-            07066653871
-          </span>
-        </div>
-        <div className="modal-action absolute right-5 -top-3 cursor-pointer">
-          <CgClose
-            className="text-2xl hover:text-red-700 "
-            onClick={closeModal}
-          />
-        </div>
+            <div className="mt-4 flex items-center justify-center gap-2">
+              <div
+                className="flex gap-2 px-2 py-0.5 border border-gray-800 rounded-lg items-center cursor-pointer"
+                onClick={() => (window.location.href = `tel:${phone}`)}
+              >
+                <IoCall className="text-xl" />
+                {phone}
+              </div>
+
+              <div
+                className="flex gap-2 px-2 py-0.5 border border-gray-800 rounded-lg items-center cursor-pointer"
+                onClick={() => (window.location.href = `mailto:${email}`)}
+              >
+                <CgMail className="text-xl" />
+                {email}
+              </div>
+            </div>
+
+            <p className="text-center text-gray-500 text-xs my-2 lowercase">
+              {business_name} grows {tuber?.join(", ")}.
+            </p>
+
+            <div className="modal-action absolute right-5 -top-3 cursor-pointer">
+              <CgClose
+                className="text-2xl hover:text-red-700"
+                onClick={closeModal}
+              />
+            </div>
+          </>
+        )}
       </div>
     </dialog>
   );
