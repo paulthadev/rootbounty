8041d8116597655691d8a216ff8315495f13aaea
fadayopaul <paulfadayo@yahoo.com>
2024-09-17 02:12:12 +0100

    Refactor ProgressBar component to simplify progress bar styling

--- src/components/dashboard/ProgressBar.jsx
+++ src/components/dashboard/ProgressBar.jsx
@@ -8,10 +8,7 @@ const ProgressBar = ({ name, percentage, unit }) => {
         {/* Container for the progress bar */}
         <div className="flex-grow bg-gray-200 h-2 rounded-full relative overflow-hidden">
           {/* Filled section */}
-          <div
-            className="bg-green-400 h-2 absolute top-0 left-0"
-            style={{ width: `${percentage}%` }}
-          ></div>
+          <div className="bg-green-400 h-2 absolute top-0 w-full left-0"></div>
           {/* Unfilled section is implicitly the background */}
         </div>
         {/* Percentage display */}
